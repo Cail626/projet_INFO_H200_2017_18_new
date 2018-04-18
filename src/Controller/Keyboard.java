46748a84175749eab1941040055925b4a5378679
Murnawful <38283399+Murnawful@users.noreply.github.com>
2018-04-18 22:39:28 +0200

    Add files via upload

--- src/Controller/Keyboard.java
+++ src/Controller/Keyboard.java
@@ -5,11 +5,13 @@ import java.awt.event.KeyListener;
 
 import Model.Game;
 import Model.Directable;
+import Moving.Player;
 
 public class Keyboard implements KeyListener {
 
     private Game game;
     private boolean inventoryState;
+    private Player p;
 
     private static final int player = 0;
 
@@ -17,6 +19,7 @@ public class Keyboard implements KeyListener {
 
     public Keyboard(Game game) {
         this.game = game;
+        this.p = game.getPlayer();
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////<KeyMethods>
@@ -25,22 +28,22 @@ public class Keyboard implements KeyListener {
     public void keyPressed(KeyEvent event) {
         int key = event.getKeyCode();
 
-        if(inventoryState == false){
+        if(! inventoryState){
             switch (key) {
                 case KeyEvent.VK_RIGHT:
-                    game.moveCharacter(1, 0, player);
+                    p.moveCharacter(1, 0);
                     break;
                 case KeyEvent.VK_LEFT:
-                    game.moveCharacter(-1, 0, player);
+                    p.moveCharacter(-1, 0);
                     break;
                 case KeyEvent.VK_DOWN:
-                    game.moveCharacter(0, 1, player);
+                    p.moveCharacter(0, 1);
                     break;
                 case KeyEvent.VK_UP:
-                    game.moveCharacter(0, -1, player);
+                    p.moveCharacter(0, -1);
                     break;
                 case KeyEvent.VK_SPACE:
-                    game.action(player);
+                    p.action();
                     break;
                 case KeyEvent.VK_P:
                     game.playerPos(player);
@@ -62,7 +65,7 @@ public class Keyboard implements KeyListener {
                     game.moveIc(Directable.NORTH);
                     break;
                 case KeyEvent.VK_SPACE:
-                    game.action(player);
+                    p.action();
                     break;
             }
         }
